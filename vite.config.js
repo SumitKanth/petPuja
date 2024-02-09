e73f543b4d80c7018f9d7117da316a9a668bc3d3
sumit <sumitkanth999@gmail.com>
2024-02-09 17:41:40 +0530

    added backend url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
       proxy:{
-        '/user': 'http://localhost:5000/api/v1',
+        // '/user': 'http://localhost:5000/api/v1',
         '/user': "https://petpujabackend.onrender.com/api/v1"
       }
   },
